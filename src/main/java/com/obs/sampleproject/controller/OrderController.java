f517c57e15459c654f1f551ad707c41580f7869e
nathanielfuad <nathanielfu.nf@gmail.com>
2023-06-13 15:18:31 +0700

    unit test orderController

--- src/main/java/com/obs/sampleproject/controller/OrderController.java
+++ src/main/java/com/obs/sampleproject/controller/OrderController.java
@@ -1,17 +1,10 @@
 package com.obs.sampleproject.controller;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import com.obs.sampleproject.util.ResponseUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.obs.sampleproject.constants.ErrorCode;
 import com.obs.sampleproject.model.input.OrderCreateInput;
@@ -43,7 +36,8 @@ public class OrderController {
 	}
 
 	@DeleteMapping
-	public ResponseEntity<Object> delete(@PathParam("id") Integer id) {
+	public ResponseEntity<Object> delete(@RequestParam("id") Integer id) {
+		System.out.println("delete");
 		return responseUtil.generate(ErrorCode.SUCCESS, orderService.deleteOrder(id));
 	}
 
---
+++ src/test/java/com/obs/sampleproject/controller/OrderControllerTest.java
@@ -0,0 +1,99 @@
+package com.obs.sampleproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.obs.sampleproject.config.ExceptionHandlerConfig;
+import com.obs.sampleproject.model.entity.Order;
+import com.obs.sampleproject.model.input.OrderCreateInput;
+import com.obs.sampleproject.model.input.OrderUpdateInput;
+import com.obs.sampleproject.repository.ItemRepository;
+import com.obs.sampleproject.repository.OrderRepository;
+import com.obs.sampleproject.service.OrderService;
+import com.obs.sampleproject.util.ResponseUtil;
+import org.aspectj.weaver.ast.Or;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(
+        controllers = {OrderController.class}
+)
+@Import({ExceptionHandlerConfig.class, ResponseUtil.class})
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private OrderService orderService;
+    @MockBean
+    private OrderRepository orderRepository;
+    @MockBean
+    private ItemRepository itemRepository;
+
+    @Test
+    void whenGetOrders_ThenStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/order"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    void whenCreateOrder_ThenStatus200() throws Exception {
+        OrderCreateInput orderCreateInput = new OrderCreateInput();
+        orderCreateInput.setItemId(1);
+        orderCreateInput.setOrderNo("O1");
+        orderCreateInput.setQty(1);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String body = objectMapper.writer().writeValueAsString(orderCreateInput);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/order")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    void whenUpdateOrder_ThenStatus200() throws Exception {
+        OrderUpdateInput orderUpdateInput = new OrderUpdateInput();
+        orderUpdateInput.setItemId(1);
+        orderUpdateInput.setOrderNo("O1");
+        orderUpdateInput.setQty(1);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String body = objectMapper.writer().writeValueAsString(orderUpdateInput);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.put("/order")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(body))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    void whenDeleteOrder_ThenStatus200() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/order")
+                                .param("id","1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+}
